229604816c4a13b7679769f88726ee24c0897c03
Austin Sanders <arsanders@ugs.gov>
2020-06-26 14:43:43 -0700

    Refactored class-based implementation into a function.

--- libpyhat/emi/emi.py
+++ libpyhat/emi/emi.py
@@ -4,80 +4,24 @@ import pandas as pd
 from pysptools.eea import FIPPI, PPI, NFINDR, ATGP
 from libpyhat.data.spectra import Spectra
 
-class EMI(object):
-    def __init__(self, data, method):
-        self.supported_formats = ("FIPPI", "PPI", "NFINDR", "ATGP")
-        self.method = method
-        self.data = data
-        self.endmembers = None
+def emi(data, emi_method, **kwargs):
+    supported_methods = ("FIPPI", "PPI", "NFINDR", "ATGP")
+    try:
+        if emi_method.upper() in supported_methods:
+            method = globals()[emi_method.upper()]()
+        else:
+            print(f"{emi_method} is not a supported method.  Supported methods are {supported_methods}")
+            return 1
+    except KeyError:
+        print(f"Unable to instantiate class from {emi_method}.")
+        return 1
 
-    def method():
-        doc = "The method used for end-member identification."
+    spectra = data['wvl'].to_numpy()
+    if len(spectra.shape) == 2:
+        spectra = np.expand_dims(spectra, 0)
 
-        def fget(self):
-            return self._method
-
-        def fset(self, value):
-            try:
-                self._method = globals()[value.upper()]()
-            except KeyError:
-                raise NotImplementedError(f"Method '{value}' is not currently supported.  "\
-                                          f"Supported formats are {self.supported_formats}")
-
-        def fdel(self):
-            del self._method
-
-        return locals()
-    method = property(**method())
-
-    def data():
-        doc = "Hyperspectral cube formatted as a numpy.ndarray."
-        def fget(self):
-            return self._data
-
-        def fset(self, value):
-            if issubclass(type(value), pd.DataFrame):
-                self._data = value.to_numpy()
-            elif issubclass(type(value), np.ndarray):
-                self._data = value
-            else:
-                raise ValueError(f"Data must inherit from pd.DataFrame or np.ndarray")
-            if len(self._data.shape) == 2:
-                self._data = np.expand_dims(self._data, 0)
-
-        def fdel(self):
-            del self._data
-
-        return locals()
-    data = property(**data())
-
-    def endmembers():
-        doc = "Endmembers identified by the algorthm"
-
-        def fget(self):
-            if self._endmembers is None:
-                print("No endmembers found. Try running .extract() before accessing endmembers.")
-            return self._endmembers
-
-        def fset(self, value):
-            if issubclass(type(value), pd.DataFrame):
-                self._endmembers = value.to_numpy()
-            elif issubclass(type(value), np.ndarray):
-                self._endmembers = value
-            elif isinstance(value, type(None)):
-                self._endmembers = None
-            else:
-                raise ValueError(f"Endmembers must inherit from pd.DataFrame or np.ndarray")
-
-        def fdel(self):
-            del self._endmembers
-
-        return locals()
-    endmembers = property(**endmembers())
-
-    def extract(self, **kwargs):
-        self.endmembers = self.method.extract(self.data, **kwargs)
-        return self.endmembers
-
-    def display(self, **kwargs):
-        self.method.display(**kwargs)
+    endmembers = method.extract(spectra, **kwargs)
+    endmember_indices = [i[0] for i in method.get_idx()]
+    indices = np.zeros(spectra.shape[1], dtype=int)
+    indices[endmember_indices] = 1
+    data[("endmembers", emi_method)] = indices
